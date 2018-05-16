233fc514bfb8ea155e235e53d28fadd56d546983
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-05-16 11:16:18 -0300

    Changed filtering to be more similar to bag-of-words approach of the Opportunity paper.

--- uci_featsel/Opportunity/filter_opportunity.pl
+++ uci_featsel/Opportunity/filter_opportunity.pl
@@ -10,18 +10,87 @@ use strict;
 #
 # Gustavo Estrela, May 2nd, 2018.
 
+# Mapping actions to 31 features
+# 
+# features  0 - 3  represent Locomotion
+# features  4 - 16 represent Left arm action
+# features 17 - 29 represent Right arm action
+#
+# There are 5 activities 
+
+my $current_activity = 0;
+my @bag_of_actions = (0)x30;
+
+my $i = 0;
+
 while (<STDIN>)
 {
     chomp $_;
-
     my @sample_line = split " ", $_;
 
+    $i += 1;
+    
+    my $activity = max ($sample_line[244] - 100, 0);
     my $locomotion = max ($sample_line[243], 0);
+    if ($locomotion > 2)
+    {
+        $locomotion -= 1;
+    }
     my $left_arm = max ($sample_line[245] - 200, 0);
     my $right_arm = max ($sample_line[247] - 400, 0);
-    my $activity = max ($sample_line[244] - 100, 0);
 
-    print "$locomotion $left_arm $right_arm  $activity\n";
+    if ($activity != 0 and $current_activity == 0)
+    {
+        $current_activity = $activity;
+    }
+    
+    if ($current_activity != 0 and $activity == 0)
+    {
+        # print last activity
+        for my $f (@bag_of_actions)
+        {
+            print "$f ";
+        }
+        for my $i (1..5)
+        {
+            print " 0" if $i != $current_activity;
+            print " 1" if $i == $current_activity;
+        }
+        print "\n";
+
+        # start an empty bag
+        @bag_of_actions = (0)x30;
+        $current_activity = 0;
+    }
+
+    
+    if ($activity != 0 and $current_activity == $activity)
+    {
+        $bag_of_actions[$locomotion - 1] = 1 if $locomotion != 0;
+        $bag_of_actions[3 + $left_arm] = 1 if $left_arm != 0;
+        $bag_of_actions[16 + $right_arm] = 1 if $right_arm != 0;
+    }
+    elsif ($activity != 0) # and $current_activity != $activity
+    {
+        # print last activity
+        for my $f (@bag_of_actions)
+        {
+            print "$f ";
+        }
+        for my $i (1..5)
+        {
+            print " 0" if $i != $current_activity;
+            print " 1" if $i == $current_activity;
+        }
+        print "\n";
+        
+        # start new activity
+        $current_activity = $activity;
+        @bag_of_actions = (0)x30;
+        $bag_of_actions[$locomotion - 1] = 1 if $locomotion != 0;
+        $bag_of_actions[3 + $left_arm] = 1 if $left_arm != 0;
+        $bag_of_actions[16 + $right_arm] = 1 if $right_arm != 0;
+    }
 }
 
 exit 0;
